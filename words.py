f3d9cf784f4a3bd3b8f88528b869c8ba652047c2
Feidhlim O'Neill <feidhlimoneill@gmail.com>
2013-05-06 01:30:01 +0100

    sort and tolower

--- words.py
+++ words.py
@@ -1,7 +1,9 @@
 my_list=[]
 file = open("words.txt",'r')
 for line in file:
-	print line,
 	line_no_cr = line.strip("\r\n")
-	my_list.append(line_no_cr),
-print my_list[0]
+	my_list.append(line_no_cr.lower()),
+file.close()
+my_list.sort()
+for new_line in my_list:
+	print new_line
